50580d40d456aebf6e55af85e995f0f637a591b1
knakamura <c60evaporator@gmail.com>
2023-06-20 23:46:43 -0700

    cv_eval_setの改造に対応

--- lightgbm_sklearn_api_3rd_crossvalscore.py
+++ lightgbm_sklearn_api_3rd_crossvalscore.py
@@ -1,10 +1,10 @@
-# %% Scikit-Learn API + 旧指定方法(early_stopping_rounds) + cross_val_score_eval_setメソッドでクロスバリデーション
+#%%
 import pandas as pd
 import numpy as np
 from sklearn.datasets import fetch_california_housing
-import lightgbm as lgb
 from sklearn.model_selection import KFold
-from seaborn_analyzer._cv_eval_set_old import cross_val_score_eval_set
+import lightgbm as lgb
+from seaborn_analyzer import cross_val_score_eval_set
 
 # データセット読込(カリフォルニア住宅価格)
 TARGET_VARIABLE = 'price'  # 目的変数名
@@ -21,26 +21,25 @@ cv = KFold(n_splits=5, shuffle=True, random_state=42)
 ###### ここからがLightGBMの実装 ######
 # 使用するパラメータ
 params = {'objective': 'regression',  # 最小化させるべき損失関数
-        'random_state': 42,  # 乱数シード
-        'boosting_type': 'gbdt',  # boosting_type
-        'n_estimators': 10000  # 最大学習サイクル数。early_stopping使用時は大きな値を入力
-        }
-verbose_eval = 0  # この数字を1にすると学習時のスコア推移がコマンドライン表示される
+          'metric': 'rmse',  # 学習時に使用する評価指標(early_stoppingの評価指標にも同じ値が使用される)
+          'random_state': 42,  # 乱数シード
+          'boosting_type': 'gbdt',  # boosting_type
+          'n_estimators': 10000,  # 最大学習サイクル数。early_stopping使用時は大きな値を入力
+          'verbose': -1,  # これを指定しないと`No further splits with positive gain, best gain: -inf`というWarningが表示される
+          'early_stopping_round': 10  # ここでearly_stoppingを指定
+          }
 # early_stoppingを指定してLightGBM学習
 lgbr = lgb.LGBMRegressor(**params)
 # クロスバリデーション内部で`fit()`メソッドに渡すパラメータ
-fit_params = {'eval_metric':'rmse',
-              'eval_set':[(X, y)],
-              'early_stopping_rounds': 10,
-              'verbose': verbose_eval}
+fit_params = {'eval_set':[(X, y)]
+              }
 # クロスバリデーション実行
 scores = cross_val_score_eval_set(
-        eval_set_selection='test',  # 'test'と指定するとテストデータを'eval_set'に渡せる
+        validation_fraction='cv',  # 'test'と指定するとテストデータを'eval_set'に渡せる
         estimator=lgbr,  # 学習器
         X=X, y=y,  # クロスバリデーション分割前のデータを渡す
         scoring='neg_root_mean_squared_error',  # RMSE（の逆数）を指定
-        cv=cv, verbose=verbose_eval, fit_params=fit_params
+        cv=cv, fit_params=fit_params
         )
 print(f'RMSE={scores} \nRMSE mean={np.mean(scores)}')
-
 # %%
